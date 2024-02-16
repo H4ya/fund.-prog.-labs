0c314cc7fcff0e6515e2a6956d4fec2c0d17cafd
Haya <145580738+H4ya@users.noreply.github.com>
2024-02-16 17:42:56 +0300

    nothin

--- lab2/Task2.cpp
+++ lab2/Task2.cpp
@@ -1,51 +1,52 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
-{
 
-    int size = 4;
-    int direction;
-    int shiftNo;
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
     string array[size];
-    string newarray[size];
-    int newIndex;
-    string empty='';
-
-    cout << "Hello! this array have " << size << " strings, please enter them\n";
-    for (int i = 0; i < size; i++)
-    {
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
         cin >> array[i];
     }
-    bool validi = true;
-    while (validi)
-    {
-        cout << "\nPlease Enter: \n1 for right \n2 for left\nYour choice: ";
-        cin >> direction;
 
-        switch (direction)
-        {
-        case 1:
+    int direction;
+    cout << "Enter the direction of shift (1 for right, 2 for left): ";
+    cin >> direction;
 
-        for (int i = 0; i < size-shiftNo; i++)
-        {
-            /* code */
-        }
-        
+    int shiftNo;
+    cout << "Enter the number of cells each item will be shifted: ";
+    cin >> shiftNo;
 
+    string replaceValue = "0";
+    cout << "Enter the value that will replace discarded values (default is 0): ";
+    cin >> replaceValue;
 
-            break;
-
-        case 2:
-
-            break;
-
-        default:
-            cout << "\nInvalid direction!";
-            break;
-        }
-
-        cout << "\nThanks! now please enter the number of the direction you want it for shifting\n ";
+    string newArray[size];
+    for (int i = 0; i < size; i++) {
+        newArray[i] = replaceValue;
     }
+
+    for (int i = 0; i < size; i++) {
+        if (direction == 1) { // right shift
+            if (i + shiftNo < size) {
+                newArray[i + shiftNo] = array[i];
+            }
+        } else if (direction == 2) { // left shift
+            if (i - shiftNo >= 0) {
+                newArray[i - shiftNo] = array[i];
+            }
+        }
+    }
+
+    cout << "The shifted array is: ";
+    for (int i = 0; i < size; i++) {
+        cout << newArray[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
